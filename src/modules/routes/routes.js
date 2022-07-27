2f3827fff2d307a964c62b560921af7ee0068033
Mnats Avetisyan <avetisian.exceedteam@gmail.com>
2022-07-27 12:17:28 +0300

    add/delete-rout

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -5,10 +5,12 @@ const {
   getAllSpends,
   createSpend,
   deleteSpend,
+  editSpend,
 } = require("../controllers/spends.controller");
 
 router.get("/AllSpends", getAllSpends);
 router.post("/createSpend", createSpend);
 router.delete("/deleteSpend", deleteSpend);
+router.patch("/editSpend", editSpend);
 
 module.exports = router;
